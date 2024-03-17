6d756d146a08c401e0a06300082326a7e00672f7
Igor Gonçalves <igordossantos38@gmail.com>
2024-03-16 22:56:56 -0300

    rename setup.lua to base_setup.lua

--- lua/core/base_setup.lua
+++ lua/core/base_setup.lua
@@ -4,5 +4,8 @@ vim.opt.colorcolumn = '80'
 vim.opt.termguicolors = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = 'menuone,noinsert,noselect'
-
+vim.opt.autoindent = true
+vim.opt.cmdheight = 2
+vim.opt.ai = true
+vim.opt.si = true
 
