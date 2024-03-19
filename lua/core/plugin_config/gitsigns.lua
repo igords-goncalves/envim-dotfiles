434d43d48b5325e99c9e6935dd6a8b5593302780
Igor Gonçalves <igordossantos38@gmail.com>
2024-03-19 20:47:57 -0300

    add git commit integrations
    - Blame integrations
    - Modify icons
    - Add kunky integrations

--- lua/core/plugin_config/gitsigns.lua
+++ lua/core/plugin_config/gitsigns.lua
@@ -1,1 +1,7 @@
-require('gitsigns').setup()
+require('gitsigns').setup({
+  
+})
+
+-- keymapings
+vim.keymap.set('n', '<leader>gh', ':Gitsigns preview_hunk<CR>', {})
+vim.keymap.set('n', '<leader>gt', ':Gitsigns toggle_current_line_blame<CR>', {})
--- lua/core/plugin_config/nvim-tree.lua
+++ lua/core/plugin_config/nvim-tree.lua
@@ -1,9 +1,32 @@
 vim.g.loaded_netry = 1
 vim.g.loaded_netrwPlugin = 1
 
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  filters = {
+    dotfiles = false,
+  },
+  git = {
+    enable = true,
+    ignore = false,
+    timeout = 500,
+  },
+  renderer = {
+    icons = {
+      glyphs = {
+        git = {
+          unstaged = "",
+          staged = "S",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "U",
+          deleted = "",
+          ignored = "◌",
+        },
+      }
+    }
+  },
+})
 
 vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
-vim.keymap.set('n', '<C-l>', '<C-w>w', defaults)
-vim.keymap.set('n', '<C-h>', '<C-w>w', defaults)
-
+vim.keymap.set('n', '<C-l>', '<C-w>w')
+vim.keymap.set('n', '<C-h>', '<C-w>w')
