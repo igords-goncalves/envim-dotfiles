191ac361350d18808289db4924f6023ab65e12b4
Igor Gonçalves <igordossantos38@gmail.com>
2024-03-30 02:24:49 -0300

    adjust on comment

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -117,7 +117,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 
-		-- Formatting now by the LSP language formatter
+		-- Formatting now by the formatters
 		--		 vim.keymap.set('n', '<leader>lf', function()
 		--	      vim.lsp.buf.format { async = true }
 		--	    end, opts)
